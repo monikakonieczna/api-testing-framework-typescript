cf688854931023f2cdda073d92a9734c31bb6aa9
Monika Konieczna <konieczna.monika01@gmail.com>
2024-09-22 13:14:12 +0200

    Modification to create junit report

--- jest.config.ts
+++ jest.config.ts
@@ -1,10 +1,17 @@
-import { Config } from '@jest/types';
+import { Config } from "@jest/types";
 
 const config: Config.InitialOptions = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  setupFiles: ['<rootDir>/src/setupTests.ts'],
-  runner: "groups"
+  preset: "ts-jest",
+  testEnvironment: "node",
+  setupFiles: ["<rootDir>/src/setupTests.ts"],
+  runner: "groups",
+  reporters: [
+    "default",
+    [
+      "jest-junit",
+      { outputDirectory: "./test-results", outputName: "test-results.xml" },
+    ],
+  ],
 };
 
 export default config;
